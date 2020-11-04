6a19090c8241585faaaa4e89731af0f9ea5b07d3
royFloresNyc <royfloresnyc@gmail.com>
2020-11-04 01:33:52 -0500

    Add image_url to Users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
     has_many :cities, through: :itineraries
-    attributes :id, :user_name, :first_name, :last_name, :email_address
+    attributes :id, :user_name, :first_name, :last_name, :email_address, :image_url
 
 end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-roy = User.create(user_name: 'royFloresNyc', first_name: 'Roy', last_name: 'Flores', email_address: 'someEmail@gmail.com', password: 'test', password_confirmation: 'test')
+roy = User.create(user_name: 'royFloresNyc', first_name: 'Roy', last_name: 'Flores', email_address: 'someEmail@gmail.com', password: 'test', password_confirmation: 'test', image_url: 'http://www.royflores.net/uploads/1/0/7/6/10766844/7171871_orig.jpg')
 david = User.create(user_name: 'EeewDavid', first_name: 'David', last_name: 'Kirsch', email_address: 'email@gmail.com', password: 'test', password_confirmation: 'test')
 
 capul = City.create(city_name: 'Capul', country_name: 'Philippines', description: 'Capul, Northern Samar, Philippines', place_id: 'ChIJF3NvKeZJpzMRh6YzIYuS_2c')
