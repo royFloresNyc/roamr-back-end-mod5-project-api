f13ebb103c521647ae7e1402ab1e8be1aae4a4a0
royFloresNyc <royfloresnyc@gmail.com>
2020-12-01 17:24:26 -0500

    Modify POI query

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -9,7 +9,8 @@ class CitiesController < ApplicationController
             client_secret: ENV['amadeus_secret'] 
         })
         begin
-            resp = amadeus.reference_data.locations.points_of_interest.get( { latitude: params[:lat], longitude: params[:lng] })
+            # resp = amadeus.reference_data.locations.points_of_interest.get( { latitude: params[:lat], longitude: params[:lng] })
+            resp = amadeus.reference_data.locations.points_of_interest.get( { latitude: params[:lat], longitude: params[:lng], radius: 5, categories: 'SIGHTS' })
             render json: {poi: resp.data, authorization: resp.request.bearer_token}
         rescue Amadeus::ResponseError => error
             render json: error
